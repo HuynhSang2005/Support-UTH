ad50164270b196bb3f56e47100e81cad1d6726a1
Nguyễn Thanh Tân <me@tozydev.id.vn>
2024-11-29 21:21:36 +0700

    Init Spring Boot project with start.spring.io

---
+++ backend/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "uth-support-backend"
---
+++ backend/src/main/java/vn/id/tozydev/uthsupport/backend/UthSupportBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.id.tozydev.uthsupport.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UthSupportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UthSupportBackendApplication.class, args);
+	}
+
+}
