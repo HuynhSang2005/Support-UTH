766970bcd33e701a200e3e5ab11c30993c44b5b8
Huỳnh Sang <145689521+HuynhSang2005@users.noreply.github.com>
2024-12-01 23:19:29 +0700

    Delete project directory

--- project/src/components/auth/AuthWrapper.tsx
+++
@@ -1,7 +0,0 @@
-// AuthWrapper.tsx
-import { PropsWithChildren } from 'react';
-import { AuthProvider } from './AuthContext';
-
-export function AuthWrapper({ children }: PropsWithChildren) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
