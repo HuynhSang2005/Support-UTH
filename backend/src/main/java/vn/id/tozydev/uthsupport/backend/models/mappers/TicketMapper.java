403825516c8b79ae665a53bddf53a85b64323366
Nguyễn Thanh Tân <me@tozydev.id.vn>
2024-12-08 21:04:43 +0700

    Ignore set null when update entity

--- backend/src/main/java/vn/id/tozydev/uthsupport/backend/models/mappers/TicketMapper.java
+++ backend/src/main/java/vn/id/tozydev/uthsupport/backend/models/mappers/TicketMapper.java
@@ -1,14 +1,14 @@
 package vn.id.tozydev.uthsupport.backend.models.mappers;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 import vn.id.tozydev.uthsupport.backend.models.dtos.ticket.CreateTicketRequest;
 import vn.id.tozydev.uthsupport.backend.models.dtos.ticket.TicketResponse;
 import vn.id.tozydev.uthsupport.backend.models.dtos.ticket.UpdateTicketRequest;
 import vn.id.tozydev.uthsupport.backend.models.entities.Ticket;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(
+    componentModel = MappingConstants.ComponentModel.SPRING,
+    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface TicketMapper {
   TicketResponse toResponse(Ticket entity);
 
