d189d6fa8547cb20bcf65b8d95991c2901491c86
Nguyễn Thanh Tân <me@tozydev.id.vn>
2024-12-08 15:03:50 +0700

    Refactor

--- backend/src/main/java/vn/id/tozydev/uthsupport/backend/UthSupportBackendApplication.java
+++ backend/src/main/java/vn/id/tozydev/uthsupport/backend/UthSupportBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UthSupportBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UthSupportBackendApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(UthSupportBackendApplication.class, args);
+  }
 }
