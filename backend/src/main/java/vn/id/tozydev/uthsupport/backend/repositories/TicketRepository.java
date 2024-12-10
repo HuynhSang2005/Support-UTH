c6a6e140881be43f9792b73a6f308a9f1d365919
Nguyễn Thanh Tân <me@tozydev.id.vn>
2024-12-10 20:28:41 +0700

    Fix HSQL query

--- backend/src/main/java/vn/id/tozydev/uthsupport/backend/repositories/TicketRepository.java
+++ backend/src/main/java/vn/id/tozydev/uthsupport/backend/repositories/TicketRepository.java
@@ -7,7 +7,6 @@ import vn.id.tozydev.uthsupport.backend.models.entities.Ticket;
 public interface TicketRepository extends CrudRepository<Ticket, Long> {
   Iterable<Ticket> findAllByCreatedBy_Username(String createdByUsername);
 
-  @Query(
-      "select t from Ticket t join t.category c join fetch c.assignees a on a.username == :username")
+  @Query("select t from Ticket t inner join t.category.assignees a on a.username = :username")
   Iterable<Ticket> findAllAssignedBy(String username);
 }
