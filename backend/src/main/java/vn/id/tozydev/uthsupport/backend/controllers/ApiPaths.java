bf3ef29644f35466ced99b4000cb8ec04d658df8
Nguyễn Thanh Tân <me@tozydev.id.vn>
2024-12-10 10:31:05 +0700

    Add current user api

--- backend/src/main/java/vn/id/tozydev/uthsupport/backend/controllers/ApiPaths.java
+++ backend/src/main/java/vn/id/tozydev/uthsupport/backend/controllers/ApiPaths.java
@@ -1,6 +1,7 @@
 package vn.id.tozydev.uthsupport.backend.controllers;
 
 final class ApiPaths {
+  static final String TICKETS_ONLY = "tickets";
   static final String COMMENTS_ONLY = "comments";
   static final String REGISTER_ONLY = "register";
   static final String LOGIN_ONLY = "login";
@@ -12,11 +13,12 @@ final class ApiPaths {
 
   static final String PREFIX = "/api/v1/";
   static final String USERS = PREFIX + "users";
-  static final String TICKETS = PREFIX + "tickets";
+  static final String TICKETS = PREFIX + TICKETS_ONLY;
   static final String TICKET_COMMENT = TICKETS + "/" + TICKET_ID_PARAM + "/" + COMMENTS_ONLY;
   static final String TICKET_STATUS = TICKET_ID_PARAM + "/status";
   static final String AUTH = PREFIX + "auth";
   static final String CATEGORIES = PREFIX + "categories";
   static final String CATEGORY_ASSIGNEES = CATEGORY_ID_PARAM + "/assignees";
   static final String COMMENTS = PREFIX + COMMENTS_ONLY;
+  static final String USER = PREFIX + "user";
 }
---
+++ backend/src/main/java/vn/id/tozydev/uthsupport/backend/controllers/LoggedInUserController.java
@@ -0,0 +1,43 @@
+package vn.id.tozydev.uthsupport.backend.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import vn.id.tozydev.uthsupport.backend.models.dtos.user.UpdateUserRequest;
+import vn.id.tozydev.uthsupport.backend.models.dtos.user.UserResponse;
+import vn.id.tozydev.uthsupport.backend.services.UserService;
+
+@RestController
+@RequestMapping(ApiPaths.USER)
+@AllArgsConstructor
+public class LoggedInUserController extends BaseController {
+  private final UserService userService;
+
+  @GetMapping
+  public ResponseEntity<UserResponse> findCurrent(Authentication authentication) {
+    if (isNotAuthenticated(authentication)) {
+      return unauthorized();
+    }
+    return of(userService.findOne(authentication.getName()));
+  }
+
+  @PatchMapping
+  public ResponseEntity<UserResponse> updateCurrent(
+      Authentication authentication, @RequestBody UpdateUserRequest request) {
+    if (isNotAuthenticated(authentication)) {
+      return unauthorized();
+    }
+    request.setRole(null);
+    return of(userService.update(authentication.getName(), request));
+  }
+
+  @DeleteMapping
+  public ResponseEntity<Void> deleteCurrent(Authentication authentication) {
+    if (isNotAuthenticated(authentication)) {
+      return unauthorized();
+    }
+    userService.delete(authentication.getName());
+    return noContent();
+  }
+}
